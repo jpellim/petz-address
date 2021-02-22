b139d7325bd6323baaf4f2cfa73de42cf7810dab
Jane Montin Pellin <janepellim@ext.cvccorp.com.br>
2021-02-21 21:48:54 -0300

    implementação controle find all

--- src/main/java/br/com/petz/controller/AddressController.java
+++ src/main/java/br/com/petz/controller/AddressController.java
@@ -1,11 +1,15 @@
 package br.com.petz.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,6 +19,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.petz.request.AddressRequest;
@@ -54,5 +60,16 @@ public class AddressController {
 		return (response.isPresent()) ? new ResponseEntity<>(response.get(), HttpStatus.OK)
 				: new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
-
+ 
+	@GetMapping 
+	public ResponseEntity<List<AddressResponse>> findAllAddress(@RequestParam(defaultValue = "0") int page,
+	        @RequestParam(defaultValue = "3") int size) {
+		
+	    Pageable paging = PageRequest.of(page, size);
+	      
+		Page<AddressResponse> response = addressService.findAll(paging);
+		
+		return (response != null  ? new ResponseEntity<>(response.getContent(), HttpStatus.OK)
+				: new ResponseEntity<>(HttpStatus.NOT_FOUND));
+	}
 }
